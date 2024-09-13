a5032df938b6f1f38bf85f5afac247a3238e73ab
hcalarcon <hernaldo.alar@gmail.com>
2024-09-13 08:44:06 -0300

    mejora en periodicTask

--- src/services/periodicTask.js
+++ src/services/periodicTask.js
@@ -5,12 +5,15 @@ import {
 import getCommitRate from "./getCommitRate.js";
 
 const persona = await getRepo();
+const hora = 3600000; //una hora en milisegundo
+const periodo = hora * 12;
 
 const executeTask = async () => {
   try {
     persona.forEach(async (persona) => {
       const commitRate = await getCommitRate(persona.username, persona.repo);
       const saved = await saveRate(persona.username, commitRate);
+      console.log("tarea ejecutada");
       return saved;
     });
   } catch (error) {
@@ -20,7 +23,7 @@ const executeTask = async () => {
 
 const periodicTask = () => {
   executeTask();
-  setInterval(executeTask, 216000000);
+  setInterval(executeTask, periodo);
 };
 
 export default periodicTask;
